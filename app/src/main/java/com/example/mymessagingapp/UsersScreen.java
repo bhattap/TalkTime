9c9a1f3f1f9545a4a17e7fa9c2b773f95a817e82
Puja Bhattacharjee <stevieisme3290@gmail.com>
2019-07-16 04:23:54 -0400

    Implemented listview of all users on home screen.

--- app/src/main/java/com/example/mymessagingapp/UsersScreen.java
+++ app/src/main/java/com/example/mymessagingapp/UsersScreen.java
@@ -3,12 +3,60 @@ package com.example.mymessagingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class UsersScreen extends AppCompatActivity {
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersScreen extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView usersListView;
+    private ArrayList<String> allUsersNames;
+    private ArrayAdapter userArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_screen);
+
+        usersListView = findViewById(R.id.usersListView);
+        allUsersNames = new ArrayList<>();
+        userArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, allUsersNames);
+        getAllUsers();
+        //usersListView.setOnItemClickListener(this);
+    }
+
+    private void getAllUsers() {
+        try {
+            ParseUser.getQuery()
+                    .whereNotEqualTo("username", ParseUser.getCurrentUser())
+                    .findInBackground(new FindCallback<ParseUser>() {
+                @Override
+                public void done(List<ParseUser> parseUsersList, ParseException e) {
+                    if (parseUsersList.size()>0 && e==null) {
+                        for (ParseUser parseUser : parseUsersList) {
+                            allUsersNames.add(parseUser.getUsername());
+                        }
+                        usersListView.setAdapter(userArrayAdapter);
+                    }
+                }
+            });
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
