4be3805eeca2a18da43dc8a53f1bf24990dc311c
Puja Bhattacharjee <stevieisme3290@gmail.com>
2019-07-17 00:17:02 -0400

    Implemented messaging feature. Next add search, scroll to bottom of chat automatically, fragments for other content, as well as touching the screen to better the user experience.

--- app/src/main/java/com/example/mymessagingapp/ChatScreen.java
+++ app/src/main/java/com/example/mymessagingapp/ChatScreen.java
@@ -2,13 +2,126 @@ package com.example.mymessagingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class ChatScreen extends AppCompatActivity {
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatScreen extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView sendMessageBtn;
+    private ListView chatListView;
+    private EditText messageEditText;
+    private String recipient, sender;
+    private ArrayAdapter chatArrayAdapter;
+    private ArrayList<String> chatMessagesList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_screen);
+        Intent receivedIntent = getIntent();
+        recipient = receivedIntent.getStringExtra("recipient");
+        sender = ParseUser.getCurrentUser().getUsername();
+        setTitle(recipient);
+
+        chatListView = findViewById(R.id.chatListView);
+        messageEditText = findViewById(R.id.messageEditText);
+        sendMessageBtn = findViewById(R.id.sendMessageBtn);
+        sendMessageBtn.setOnClickListener(this);
+
+        chatMessagesList = new ArrayList<>();
+        chatArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, chatMessagesList);
+        setUpChatHistory();
+    }
+
+    private void setUpChatHistory() {
+        try {
+            //get all the chat messages sent by sender and received by recipient
+            //also get the ones sent by recipient and received by sender
+            //order these two lists
+            //display the messages on the listview
+            ParseQuery<ParseObject> sentBySender =
+                    ParseQuery.getQuery("Chat")
+                        .whereEqualTo("sender", sender)
+                        .whereEqualTo("recipient", recipient);
+            ParseQuery<ParseObject> sentByRecipient =
+                    ParseQuery.getQuery("Chat")
+                            .whereEqualTo("sender", recipient)
+                            .whereEqualTo("recipient", sender);
+            //create an arraylist for the two queries
+            ArrayList<ParseQuery<ParseObject>> allQueries = new ArrayList<>();
+            allQueries.add(sentBySender);
+            allQueries.add(sentByRecipient);
+
+            //create a new query to order our arraylist
+            ParseQuery<ParseObject> myQuery = ParseQuery.or(allQueries);
+            myQuery.orderByAscending("createdAt");
+            myQuery.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> objects, ParseException e) {
+                    if (objects.size()>0 && e==null){
+                        for (ParseObject message : objects){
+                            chatMessagesList.add(
+                                    message.get("sender")+ ": "+ message.get("message"));
+                        }
+                        chatListView.setAdapter(chatArrayAdapter);
+                    }
+                }
+            });
+
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case (R.id.sendMessageBtn):
+                if (!messageEditText.getText().toString().isEmpty()) {
+                    //add the new message to the server
+                    ParseObject newChatObject = new ParseObject("Chat");
+                    newChatObject.put("sender", sender);
+                    newChatObject.put("recipient", recipient);
+                    newChatObject.put("message", messageEditText.getText().toString());
+                    newChatObject.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e==null){
+                                //display on the screen
+                                chatMessagesList.add(sender+": "+messageEditText.getText());
+                                chatArrayAdapter.notifyDataSetChanged();
+
+                                //clear out the edit text field
+                                messageEditText.setText("");
+                            } else {
+                                FancyToast.makeText(ChatScreen.this, e.getMessage(),
+                                        Toast.LENGTH_LONG, FancyToast.ERROR,
+                                        false).show();
+                            }
+                        }
+                    });
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/mymessagingapp/UsersScreen.java
+++ app/src/main/java/com/example/mymessagingapp/UsersScreen.java
@@ -2,16 +2,26 @@ package com.example.mymessagingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
+import com.parse.LogOutCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,19 +36,51 @@ public class UsersScreen extends AppCompatActivity implements AdapterView.OnItem
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_screen);
+        setTitle("All Users");
 
         usersListView = findViewById(R.id.usersListView);
         allUsersNames = new ArrayList<>();
         userArrayAdapter = new ArrayAdapter(this,
                 android.R.layout.simple_list_item_1, allUsersNames);
         getAllUsers();
-        //usersListView.setOnItemClickListener(this);
+        usersListView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case (R.id.logoutBtn):
+                ParseUser.logOutInBackground(new LogOutCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e==null){
+                            FancyToast.makeText(UsersScreen.this, "You are now logged out",
+                                    Toast.LENGTH_SHORT, FancyToast.INFO, false).show();
+
+                            Intent intent = new Intent(UsersScreen.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void getAllUsers() {
         try {
             ParseUser.getQuery()
-                    .whereNotEqualTo("username", ParseUser.getCurrentUser())
+                    .whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername())
                     .findInBackground(new FindCallback<ParseUser>() {
                 @Override
                 public void done(List<ParseUser> parseUsersList, ParseException e) {
@@ -57,6 +99,8 @@ public class UsersScreen extends AppCompatActivity implements AdapterView.OnItem
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+        Intent intent = new Intent(this, ChatScreen.class);
+        intent.putExtra("recipient", allUsersNames.get(position));
+        startActivity(intent);
     }
 }
